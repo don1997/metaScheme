28de061fc912af28e333c90d650d8765a516720c
don1997 <donald_j97@hotmail.com>
2022-09-05 17:30:59 -0400

    added io

---
+++ io.rkt
@@ -0,0 +1,50 @@
+;; What is this??
+;; It is a bare minimum way to get input and output in the repl and essentially can only eval strings and numbers.
+;; Basically a pretty printer but I wanted to see how to get I/O working in a bare minimum system.
+
+#lang racket
+
+;; mini eval
+(define (eval exp)
+  (cond
+  ((self-eval? exp) exp)
+  (else
+   error "UKNOWN")))
+
+;; Selector self-eval?
+(define (self-eval? exp)
+  (cond
+    ((number? exp) #t)
+    ((string? exp) #t)
+    (else #f)))
+
+;; I/O stuff....
+
+(define input-prompt ";;; M-Eval input:")
+
+(define (prompt-for-input string)
+  (newline)
+  (newline)
+  (display string)
+  (newline))
+
+(define output-prompt ";;; M-Eval value:")
+
+(define (announce-output string)
+  (newline)
+  (display string)
+  (newline))
+
+(define (user-print object)
+      (display object))
+
+(define (driver-loop)
+   (prompt-for-input input-prompt)
+   (let ([input (read)])
+     (let ([output (eval input)])
+       (announce-output output-prompt)
+       (user-print output)))
+   (driver-loop))
+
+;;init interpreter
+(driver-loop)
